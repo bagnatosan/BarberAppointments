8e8878043848e104d5d3fab1921af03053b095bf
bagnatosan <bagnatosan@gmail.com>
2026-05-27 15:44:00 -0300

    tmp

--- obj/Debug/net8.0/linux-x64/Barber.AssemblyInfo.cs
+++ obj/Debug/net8.0/linux-x64/Barber.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d8547545eb0660258d51494eed7ad92ab18a350")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+449b1daf70d07ade754395b5465dc7fc6d077e05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
